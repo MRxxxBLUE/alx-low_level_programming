f8a862ed6732aa33c81b5a29ff8393a901255d12
Eslam Ahmed <ea5537795@gmail.com>
2023-07-18 13:45:21 -0700

    hello c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: prints all alphapet in lowercase
+ *
+ * Return: Always 0 (success)
+*/
+
+void print_alphabet(void);
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+	_putchar('\n');
+
+	return (0);
+}
