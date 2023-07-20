26155b2af2bed2a3500d4253e85845a70adea105
Eslam Ahmed <ea5537795@gmail.com>
2023-07-20 16:40:24 -0700

    new C

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _mul - multiplies two integrs
+ *
+ * @a: first input
+ * @b: second input
+ *
+ * Return: return result
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
