47185afe9b6d7e8e250b41248c768e12d712ef9c
Eslam Ahmed <ea5537795@gmail.com>
2023-07-20 16:52:57 -0700

    new C

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * _numbers - print from 0 to 9
- *		only use _putchar twice
+ * print_numbers - print 0 - 9
+ *		only using _putchar twice
  *
  * Return: Always 0 (success)
 */
