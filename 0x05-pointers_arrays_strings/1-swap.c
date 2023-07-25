0bd626a904b8ad90f0ef508a259a95215b9f4588
Eslam Ahmed <ea5537795@gmail.com>
2023-07-25 12:01:06 -0700

    second

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * swap_int - swape the values of two integers
+ *		using two input parameters
+ *
+ * @a: input parameter 1
+ * @b: input parameter 2
+ *
+ * return: nothing
+*/
+
+void swap_int(int *a, int *b)
+{
+	int temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
