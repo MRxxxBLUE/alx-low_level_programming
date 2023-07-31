7d394836be1977c6d4f0acc1d8759cd492b2128e
Eslam Ahmed <ea5537795@gmail.com>
2023-07-31 15:41:28 +0200

    more & more

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+#include "main.h"
+/**
+ * _strpbrk - Entry point
+ * @s: input
+ * @accept: input
+ * Return: Always 0 (Success)
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int ser, chh;
+	char *p;
+
+	ser = 0;
+	while (s[ser] != '\0')
+	{
+		ser = 0;
+		while (accept[chh] != '\0')
+		{
+			if (accept[chh] == s[ser])
+			{
+				p = &s[ser];
+				return (p);
+			}
+			chh++;
+		}
+		ser++
+	}
+
+	return (0);
+}
+
+
+
+}
