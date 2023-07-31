537f9b355f4f2f1a966099b0186a926ae8595576
Eslam Ahmed <ea5537795@gmail.com>
2023-07-31 16:09:27 +0200

    more & more

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_chessboard - Entry point
+ * @a: array
+ * Return: Always 0 (Success)
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int ser, chh;
+
+	for (ser = 0; ser < 8, ser++)
+	{
+		for (chh = 0; chh < 8, chh++)
+		{
+			_putchar(a[ser][chh]);
+		}
+		_putchar('\n');
+	}
+}
